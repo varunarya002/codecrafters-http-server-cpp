94f492016e7adece7420c3616c7e8773f44288e9
Varun Arya <varun.arya002@gmail.com>
2024-10-06 13:16:36 +0530

    Sent response to client

--- src/server.cpp
+++ src/server.cpp
@@ -18,7 +18,7 @@ int main(int argc, char **argv) {
 
   // Uncomment this block to pass the first stage
 
-  int server_fd = socket(AF_INET, SOCK_STREAM, 0); //AF->ADDRESS FAMILY
+  int server_fd = socket(AF_INET, SOCK_STREAM, 0); //AF->ADDRESS FAMILY; By specifying 0, you allow the system to choose the default protocol for the given socket type, which is TCP for SOCK_STREAM
   if (server_fd < 0) {
    std::cerr << "Failed to create server socket\n";
    return 1;
@@ -53,8 +53,12 @@ int main(int argc, char **argv) {
 
   std::cout << "Waiting for a client to connect...\n";
 
-  accept(server_fd, (struct sockaddr *) &client_addr, (socklen_t *) &client_addr_len);
-  std::cout << "Client connected\n";
+  const int client_fd = accept(server_fd, (struct sockaddr *) &client_addr, (socklen_t *) &client_addr_len);
+  std::cout << "Client connected with fd" << client_fd << "\n" ;
+
+  const char* response = "HTTP/1.1 200 OK\r\n\r\n";
+  send(client_fd, response, strlen(response), 0);
+  std::cout << "Response sent successfully!\n";
 
   close(server_fd);
 
