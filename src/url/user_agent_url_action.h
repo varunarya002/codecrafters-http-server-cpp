dfb0bea89e6a3b5d5363628f33baf61794df7400
Varun Arya <varun.arya002@gmail.com>
2024-11-16 02:48:18 +0530

    Add gzip support

--- src/url/user_agent_url_action.h
+++ src/url/user_agent_url_action.h
@@ -11,7 +11,7 @@ public:
 
     [[nodiscard]] HttpResponse execute(const HttpRequest &http_request) const override {
         const std::string message = "OK";
-        std::string body = http_request.headers.at(http_request.path);
+        std::string body = http_request.headers.at("User-Agent");
         return HttpResponse(message, 200, "text/plain", body.length(), body, http_request.headers);
     }
 };
